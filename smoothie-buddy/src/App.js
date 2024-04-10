fa22ea165f8dc5f364c1412ab83d4c35e38782de
nikitakabir <nikitakabir123@gmail.com>
2024-04-09 20:27:40 -0500

    adds prototype-user-preferences to a new branch

--- smoothie-buddy/src/App.js
+++ smoothie-buddy/src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { Card } from 'primereact/card';
+import { Panel } from 'primereact/panel';
+//import { Splitter, SplitterPanel } from 'primereact/splitter';
+import React, { useLayoutEffect } from 'react'
+import "primereact/resources/themes/lara-dark-purple/theme.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Card title="Dietary Restrictions">
+            <p className="m-0">
+        <Panel header="Macro Goals">
+            <p className="m-0">
+                Protein 60g
+            </p>
+        </Panel>
+      </p>
+      </Card>
+        <Card title="Dietary Restrictions">
+            <p className="m-0" /*style = {{boxShadow: "10px 10px 5px pink", height: "60px"}*/>
+              gluten free
+              <Card>
+                <p className="middle-card">
+                dairy free 
+                </p>
+              </Card>
+            </p>
+        </Card>
+        <Panel header="Weight Goals">
+            <p className="m-0">
+                Lose 10 lbs 
+            </p>
+        </Panel>
+        <Panel header="BMI">
+            <p className="m-0">
+                Your BMI Score: 92
+            </p>
+        </Panel>
+        <Panel header="Your Main Vitamins and Minerals">
+            <p className="m-0">
+                Vitamins 
+            </p>
+        </Panel>
       </header>
     </div>
   );
