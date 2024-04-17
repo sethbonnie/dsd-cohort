fba0b65b3e03b3b3a87d52cc945f798c5b4ba2e0
Brendan Huddleston <brendanhuddleston18@gmail.com>
2024-04-17 10:59:10 -0500

    adds initial attempt at finding ingredients by recipe and parsing data into something usable

---
+++ frontend/src/components/NutritionFacts.jsx
@@ -0,0 +1,39 @@
+import ingredients from '../data/ingredients.json'
+import nutritionData from '../data/nutrition_data.json'
+
+
+// Need the parent to pass down what recipe user wants to see the nutrition facts for
+export default function NutritionFacts(props){
+    // Bring in the desired recipe from user
+    // Find the ingredients for the recipe in the ingredients.json
+    // parse through the nutritionData.json and extract the nutrition data for each ingredient
+    // Sum the nutrition values and render on screen 
+    
+    function findIngredients(recipe){
+        let specifiedIngredients = ingredients['ingredients by recipe'][recipe]["main"];
+        return specifiedIngredients;
+    };
+
+    function parseNutritionData(recipe){
+        let parsedData = {};
+        let specifiedIngredients = findIngredients(recipe);
+
+        for (let i = 0; i <= specifiedIngredients.length; i++){
+            console.log(i)
+            for (let j = 0; j <= nutritionData.length; j++){
+                if(specifiedIngredients[i] === nutritionData[j]["ingredients"][0]["text"]){
+                    let nutritionValues = nutritionData[j]["ingredients"][0]["text"];
+                    parsedData[specifiedIngredients[i]] = nutritionValues;
+                };
+            };
+        };
+        console.log(parsedData)
+    };
+
+    parseNutritionData("Green Machine Smoothie")
+    return(
+
+        <h1>Hello World</h1>
+
+    );
+};
