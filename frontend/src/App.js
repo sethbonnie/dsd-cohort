78e4c9ad708079ba4f09af04a4fef90077a189a9
Aaron <airboy998@msn.com>
2024-04-18 21:05:50 -0500

    Add my kitchen page (#8)
    * first edit to readme.md
    
    * add test to ./frontend.App.js
    
    * add primeicons
    
    * add .gitignore to user-inventory prototype
    
    * add kitchen page to frontend
    
    * remove comments as per PR
    
    ---------
    
    Co-authored-by: Airboy98 <Airboy98@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Kitchen from "./pages/Kitchen.js";
+import logo from "./logo.svg";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
@@ -9,24 +10,10 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/kitchen" />
+          <Route index element={<div>recipe screen</div>}/>
+          <Route path="/kitchen" element={<Kitchen />}/>
         </Routes>
       </BrowserRouter>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        test hello world
-      </header>
     </div>
   );
 }
