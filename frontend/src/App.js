73a504aa32c32b0d4f7d46baf6cbacd58a101981
Aaron <airboy998@msn.com>
2024-04-18 20:14:25 -0500

    Frontend navbar (#11)
    * first edit to readme.md
    
    * add test to ./frontend.App.js
    
    * add basic navbar with logo, font, and dummy link to my kitchen
    
    * remove burger menu code, add colors to logo text, embed Navbar component into App.js
    
    ---------
    
    Co-authored-by: Airboy98 <Airboy98@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/kitchen" />
+        </Routes>
+      </BrowserRouter>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -17,6 +25,7 @@ function App() {
         >
           Learn React
         </a>
+        test hello world
       </header>
     </div>
   );
