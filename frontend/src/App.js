905669ec03b01543c4bf828dca8c71fac7688268
Airboy98 <Airboy98@users.noreply.github.com>
2024-04-20 14:40:00 -0500

    feat: initial commit for grocery list component (KAN-27)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,18 @@ import Kitchen from "./pages/Kitchen.js";
 import logo from "./logo.svg";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import NutritionFacts from './components/NutritionFacts';
+import NutritionFacts from "./components/NutritionFacts";
+import Grocerylist from "./components/Grocerylist.js";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
+        <Grocerylist />
         <Routes>
-          <Route index element={<div>recipe screen</div>}/>
-          <Route path="/kitchen" element={<Kitchen />}/>
+          <Route index element={<div>recipe screen</div>} />
+          <Route path="/kitchen" element={<Kitchen />} />
         </Routes>
       </BrowserRouter>
     </div>
